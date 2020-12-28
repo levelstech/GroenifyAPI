b7645b2e9c06f195b983706518d1a07df7cd3382
ReadyRectionz <leroyvelzel@gmail.com>
2020-12-28 23:45:33 +0100

    Updated Resolver for E Pole

---
+++ src/main/java/com/groenify/api/framework/annotation/resolver/EPoleInPathResolver.java
@@ -0,0 +1,39 @@
+package com.groenify.api.framework.annotation.resolver;
+
+import com.groenify.api.database.epole.EPole;
+import com.groenify.api.database.epole.EPoleBrand;
+import com.groenify.api.exceptions.PathException;
+import com.groenify.api.framework.annotation.EPoleBrandInPath;
+import com.groenify.api.framework.annotation.EPoleInPath;
+import com.groenify.api.repository.epole.EPoleBrandRepository;
+import com.groenify.api.repository.epole.EPoleRepository;
+import com.groenify.api.util.ResolverUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Optional;
+
+@Service
+public class EPoleInPathResolver
+        extends AbstractMethodArgumentResolver<EPoleInPath> {
+
+    private final EPoleRepository repository;
+
+    public EPoleInPathResolver(final EPoleRepository var) {
+        super(EPoleInPath.class);
+        this.repository = var;
+    }
+
+    @Override
+    protected final EPole resolver(
+            final NativeWebRequest var3, final EPoleInPath annotation)
+            throws PathException {
+
+        final Long pathValue = ResolverUtil
+                .findLongInPath(var3, annotation.value());
+        final Optional<EPole> ePole = repository.findById(pathValue);
+        if (ePole.isPresent()) return ePole.get();
+
+        throw PathException.notFoundWithId(EPole.class, pathValue);
+    }
+}
