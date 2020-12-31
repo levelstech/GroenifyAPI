35eeb262ae4b8fb6938649b64245ffaea94d9b44
ReadyRectionz <leroyvelzel@gmail.com>
2020-12-31 10:28:37 +0100

    Added Factor Type Repository

---
+++ src/main/java/com/groenify/api/repository/factor/FactorTypeRepository.java
@@ -0,0 +1,8 @@
+package com.groenify.api.repository.factor;
+
+import com.groenify.api.database.factor.Factor;
+import com.groenify.api.database.factor.FactorType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FactorTypeRepository extends CrudRepository<FactorType, Long> {
+}
