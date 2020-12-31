bfb67ea431a69c641cb6672d3348d290d42c6612
ReadyRectionz <leroyvelzel@gmail.com>
2020-12-31 10:29:57 +0100

    Added FactorType Service

---
+++ src/main/java/com/groenify/api/service/factor/FactorTypeService.java
@@ -0,0 +1,47 @@
+package com.groenify.api.service.factor;
+
+import com.groenify.api.database.epole.EPoleBrand;
+import com.groenify.api.database.factor.FactorType;
+import com.groenify.api.repository.epole.EPoleBrandRepository;
+import com.groenify.api.repository.factor.FactorTypeRepository;
+import com.groenify.api.rest.epole.__model.EPoleBrandReqMo;
+import com.groenify.api.util.ListUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FactorTypeService {
+
+    private final FactorTypeRepository repository;
+
+    public FactorTypeService(final FactorTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    public FactorTypeRepository getRepository() {
+        return repository;
+    }
+
+    public List<FactorType> getAll() {
+        final Iterable<FactorType> allFactorTypeInIter = repository.findAll();
+        return ListUtil.iterableToList(allFactorTypeInIter);
+    }
+
+    public FactorType create(final FactorTypeReqMo body) {
+        final FactorType type = FactorType.ofReqMo(body);
+        return repository.save(type);
+    }
+
+    public EPoleBrand update(
+            final FactorType type,
+            final FactorTypeReqMo body) {
+        return repository.save(type.update(body));
+    }
+
+    public Boolean delete(final FactorType type) {
+        final Long id = type.getId();
+        repository.delete(type);
+        return !repository.existsById(id);
+    }
+}
