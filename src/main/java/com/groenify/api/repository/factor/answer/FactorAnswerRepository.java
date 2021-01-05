620479c3ffd74e35492a89d8fabb47db66228f47
ReadyRectionz <leroyvelzel@gmail.com>
2021-01-05 11:31:48 +0100

    Updated AnswerRepository

--- src/main/java/com/groenify/api/repository/factor/answer/FactorAnswerRepository.java
+++ src/main/java/com/groenify/api/repository/factor/answer/FactorAnswerRepository.java
@@ -1,8 +1,13 @@
-package com.groenify.api.repository.factor;
+package com.groenify.api.repository.factor.answer;
 
+import com.groenify.api.database.factor.Factor;
 import com.groenify.api.database.factor.answer.FactorAnswer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FactorAnswerRepository
         extends CrudRepository<FactorAnswer, Long> {
+
+    List<FactorAnswer> findAllByFactor(final Factor factor);
 }
