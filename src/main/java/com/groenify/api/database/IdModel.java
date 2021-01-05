430054f0d32a54d7244cd5b9a98f5ea0d9372f9f
ReadyRectionz <leroyvelzel@gmail.com>
2021-01-05 11:12:11 +0100

    Added ID Model equals

--- src/main/java/com/groenify/api/database/IdModel.java
+++ src/main/java/com/groenify/api/database/IdModel.java
@@ -1,7 +1,13 @@
 package com.groenify.api.database;
 
+import java.util.Objects;
+
 public interface IdModel {
     Long getId();
 
     void setId(Long var);
+
+    default Boolean equalsId(final IdModel model) {
+        return Objects.equals(this.getId(), model.getId());
+    }
 }
