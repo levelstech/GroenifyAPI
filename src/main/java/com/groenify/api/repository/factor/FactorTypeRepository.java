60a81054595f9e56ba6c2ba8fe7d058a90917505
ReadyRectionz <leroyvelzel@gmail.com>
2021-01-05 11:13:11 +0100

    Updated FactorTypeRepository

--- src/main/java/com/groenify/api/repository/factor/FactorTypeRepository.java
+++ src/main/java/com/groenify/api/repository/factor/FactorTypeRepository.java
@@ -3,7 +3,11 @@ package com.groenify.api.repository.factor;
 import com.groenify.api.database.factor.FactorType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface FactorTypeRepository extends CrudRepository<FactorType, Long> {
 
     Boolean existsByNameIgnoreCase(String var);
+
+    Optional<FactorType> findByNameIgnoreCase(String var);
 }
