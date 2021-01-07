1b2fb1cc3c2f446be32a1371fa41489e0a36eddd
ReadyRectionz <leroyvelzel@gmail.com>
2021-01-07 22:20:35 +0100

    Added Repository answer

---
+++ src/main/java/com/groenify/api/repository/company/CompanyEPoleFactorAnswerRepository.java
@@ -0,0 +1,8 @@
+package com.groenify.api.repository.company;
+
+import com.groenify.api.database.company.CompanyEPoleFactorAnswer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyEPoleFactorAnswerRepository
+        extends CrudRepository<CompanyEPoleFactorAnswer, Long> {
+}
